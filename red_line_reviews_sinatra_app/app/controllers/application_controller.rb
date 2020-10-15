652afaf557176796d065e9b50cbb6710fb443bd3
Chance Smith <chancesmith97@hotmail.com>
2020-10-15 23:50:57 +0000

    added signup form and show page for users + added sign up to users_controller

---
+++ red_line_reviews_sinatra_app/app/controllers/application_controller.rb
@@ -0,0 +1,23 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "destroy_all_humans"
+  end
+
+  get "/" do
+    erb :welcome
+  end
+  
+  helpers do 
+    def current_user 
+      User.find_by(id: session[:user_id])  
+    end
+    
+  end
+
+end
